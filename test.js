9b77cee037708d8e95e4472cc022f4fa6af1741a
blueviolet <bjy0212@gmail.com>
2021-06-02 00:42:26 +0900

    init

--- test.js
+++ test.js
@@ -215,5 +215,5 @@ testData = {
     }
 };
 
-preload('1,2,3,4,5,6,7,8,9,10,11,12'.split(',').map(e => `assets/${e}.png`));
+preload(["assets/1.png", "assets/2.png", "assets/3.jpg", "assets/4.png", "assets/5.png", "assets/6.png", "assets/7.png", "assets/8.png", "assets/9.png", "assets/10.png", "assets/11.png", "assets/12.png"]);
 Init();
